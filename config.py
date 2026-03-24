5079e7f238df443b790ab18b99999dc651e3b421
Arman Monajemi <monajemia972@gmail.com>
2026-03-24 18:09:50 +0330

    add selected metadata keys

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 dataset_paths = ("dataset/images", "dataset/masks", "dataset/UCSF-PDGM-metadata_v2.csv")
 dataset_json_paths = ("dataset/train.json", "dataset/val.json", "dataset/test.json")
 checkpoint_path = "checkpoint.ckpt"
+selected_metadata_keys = ["MGMT status", "1p/19q", "IDH"]
